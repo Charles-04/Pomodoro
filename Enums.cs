412f246f9aea24a2743cfa5c704dcff69d8aa5e0
Charles-04 <charlesmir04@gmail.com>
2022-11-24 09:01:49 +0100

    Code refactored 😁😁

---
+++ Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pomodoro
+{
+    public static class Enums
+    {
+        public enum CommonNumbersEnum
+        {
+            delayTime = 2000,
+            frequency = 3276,
+            countDownTime = 1000,
+            sixtySecs = 60
+
+        }
+    }
+}
--- Pomodoro.cs
+++ Pomodoro.cs
@@ -25,14 +25,22 @@
 
             Pomodoro pomodoro = new();
 
-            if (int.TryParse(workDurationInput, out pomodoro._workDuration) && int.TryParse(restDurationInput, out pomodoro._restDuration))
-
+            try
             {
+                pomodoro._restDuration = int.Parse(restDurationInput);
+               pomodoro._workDuration = int.Parse(workDurationInput);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Input Format");
+                this.RestartPomodoro();
+            }
+
 
 
                 Console.ForegroundColor = ConsoleColor.Black;
                 Console.BackgroundColor = ConsoleColor.White;
-                
+
                 var workTime = new DateTime(2000, 1, 1, 0, pomodoro._workDuration, 0);
                 var restTime = new DateTime(2000, 1, 1, 0, pomodoro._restDuration, 0);
 
@@ -44,11 +52,11 @@
                     string workStart = Convert.ToString(WorkStartTime);
                     _workStartLogs.Add($"Started working at : {workStart}");
                     Console.CursorVisible = false;
-                    for (int i = 0; i <= pomodoro._workDuration * 60; i++)
+                    for (int i = 0; i <= pomodoro._workDuration * (int)Enums.CommonNumbersEnum.sixtySecs; i++)
                     {
                         Console.Write("Work Time Remains : {0}", workTime.ToString("mm:ss"));
                         workTime = workTime.AddSeconds(-1);
-                        Thread.Sleep(1000);
+                        Thread.Sleep((int)Enums.CommonNumbersEnum.countDownTime);
                         Console.Clear();
                     }
 
@@ -56,11 +64,11 @@
                     string workStop = Convert.ToString(WorkStopTime);
                     _workStopLogs.Add($"Stopped working at : {workStop}");
 
-                    Console.Beep(3276, 2000);
+                    Console.Beep((int)Enums.CommonNumbersEnum.frequency, (int)Enums.CommonNumbersEnum.delayTime);
                     Console.WriteLine("Work Time Over");
                     Console.WriteLine("Rest Time Starts");
-                    
-                    Thread.Sleep(2000);
+
+                    Thread.Sleep((int)Enums.CommonNumbersEnum.delayTime);
                     Console.Clear();
                 }
                 WorkPomodoro();
@@ -71,21 +79,23 @@
                     _restStartLogs.Add($"Started resting at : {restStart}");
 
                     Console.CursorVisible = false;
-                    for (int i = 0; i <= pomodoro._restDuration * 60; i++)
+                    for (int i = 0; i <= pomodoro._restDuration * (int)Enums.CommonNumbersEnum.sixtySecs; i++)
                     {
                         Console.Write("Rest Time Remains : {0}", restTime.ToString("mm:ss"));
                         restTime = restTime.AddSeconds(-1);
-                        Thread.Sleep(1000);
+                        Thread.Sleep((int)Enums.CommonNumbersEnum.countDownTime);
                         Console.Clear();
                     }
 
-                    Console.Beep(3276, 2000);
+                    Console.Beep((int)Enums.CommonNumbersEnum.frequency, (int)Enums.CommonNumbersEnum.delayTime);
                     DateTime RestStopTime = DateTime.Now;
                     string restStop = Convert.ToString(RestStopTime);
                     _restStopLogs.Add($"Stopped resting at : {restStop}");
 
-                   
-                    Console.WriteLine("Rest Time Over, Press Y to start over or any other key to terminate");
+
+                    Console.WriteLine("Rest Time Over");
+                    Console.WriteLine("Press Y to continue or any other key to exit");
+
                     var Option = Console.ReadLine().ToUpper();
 
                     if (Option == "Y")
@@ -94,21 +104,23 @@
                     }
                     else
                     {
-                        DisplayLogs();
+
                         return;
                     }
+
 
                 }
 
                 RestPomodoro();
-            }
-            else
-            {
-                Console.WriteLine("Wrong Input Format!!!");
-                RestartPomodoro();
-            }
 
 
+
+                DisplayLogs();
+            
+            
+
+
+             
         }
       
         private void DisplayLogs()
@@ -128,6 +140,8 @@
         
 
         private void RestartPomodoro() => PomodoroRule();
+        
+
         
     }
 }
